fix: Fixed PegasusRBM gradients not working

models/autoencoders/gumboltCaloPRBM.py
@@ -85,16 +85,17 @@ class GumBoltCaloPRBM(gcv6.GumBoltCaloV6):
 
         :return energy expectation value over the current batch
         """
-        w_dict, b_dict = self.prior.weight_dict.copy(), \
-            self.prior.bias_dict.copy()
-        
+        w_dict = self.prior.weight_dict
+        b_dict = self.prior.bias_dict
+
+        w_dict_cp = {}
+
         # Broadcast weight matrices (n_nodes_pa, n_nodes_pb) to
         # (batch_size, n_nodes_pa, n_nodes_pb)
         for key in w_dict.keys():
-            w_dict[key] = torch.nn.Parameter(w_dict[key] +
-                                             torch.zeros((p0_state.size(0),) +
-                                            w_dict[key].size(),
-                                            device=w_dict[key].device))
+            w_dict_cp[key] = w_dict[key] + torch.zeros((p0_state.size(0),) +
+                                                    w_dict[key].size(),
+                                                    device=w_dict[key].device)
 
         # Prepare px_state_t for torch.bmm()
         # Change px_state.size() to (batch_size, 1, n_nodes_px)
@@ -114,17 +115,17 @@ class GumBoltCaloPRBM(gcv6.GumBoltCaloV6):
             torch.matmul(p2_state, b_dict['2']) - \
             torch.matmul(p3_state, b_dict['3']) - \
             torch.bmm(p0_state_t,
-                      torch.bmm(w_dict['01'], p1_state_i)).reshape(-1) - \
+                      torch.bmm(w_dict_cp['01'], p1_state_i)).reshape(-1) - \
             torch.bmm(p0_state_t,
-                      torch.bmm(w_dict['02'], p2_state_i)).reshape(-1) - \
+                      torch.bmm(w_dict_cp['02'], p2_state_i)).reshape(-1) - \
             torch.bmm(p0_state_t,
-                      torch.bmm(w_dict['03'], p3_state_i)).reshape(-1) - \
+                      torch.bmm(w_dict_cp['03'], p3_state_i)).reshape(-1) - \
             torch.bmm(p1_state_t,
-                      torch.bmm(w_dict['12'], p2_state_i)).reshape(-1) - \
+                      torch.bmm(w_dict_cp['12'], p2_state_i)).reshape(-1) - \
             torch.bmm(p1_state_t,
-                      torch.bmm(w_dict['13'], p3_state_i)).reshape(-1) - \
+                      torch.bmm(w_dict_cp['13'], p3_state_i)).reshape(-1) - \
             torch.bmm(p2_state_t,
-                      torch.bmm(w_dict['23'], p3_state_i)).reshape(-1)
+                      torch.bmm(w_dict_cp['23'], p3_state_i)).reshape(-1)
 
         return torch.mean(batch_energy, dim=0)
 

scripts/run.py
@@ -45,14 +45,18 @@ from models.modelCreator import ModelCreator
 
 @hydra.main(config_path="../configs", config_name="config")
 def main(cfg=None):
-    #initialise wandb logging. Note that this function has many more options,
-    #reference: https://docs.wandb.ai/ref/python/init
-    #this is the setting for individual, ungrouped runs
+    # initialise wandb logging. Note that this function has many more options,
+    # reference: https://docs.wandb.ai/ref/python/init
+    # this is the setting for individual, ungrouped runs
+    # Use mode='disabled' to prevent logging
     wandb.init(project="caloqvae", entity="qvae", config=cfg)
-    #run the ting
+    # run the ting
     run(config=cfg)
 
 def run(config=None):
+    """
+    Run m
+    """
 
     #create model handling object
     modelCreator=ModelCreator(cfg=config)
@@ -70,7 +74,7 @@ def run(config=None):
         modelCreator.default_activation_fct=torch.nn.Tanh()
     else:
         logger.warning("Setting identity as default activation fct")
-        modelCreator.default_activation_fct=torch.nn.Identity() 
+        modelCreator.default_activation_fct=torch.nn.Identity()
 
     #instantiate the chosen model
     #loads from file 
@@ -79,6 +83,9 @@ def run(config=None):
     model.create_networks()
     #Not printing much useful info at the moment to avoid clutter. TODO optimise
     model.print_model_info()
+
+    for name, param in model.named_parameters():
+        print(name, param.requires_grad)
 
     # Load the model on the GPU if applicable
     dev = None
@@ -147,6 +154,7 @@ def run(config=None):
         modelCreator.save_state(config_string)
 
     logger.info("run() finished successfully.")
+
 
 if __name__=="__main__":
     logger.info("Starting main executable.")

utils/hists/showerdepthhist.py
@@ -27,7 +27,8 @@ class ShowerDepthHist(object):
         
         layer_energies = {}
         for layer, layer_idxs in self._layer_dict.items():
-            layer_energies[layer] = [dataset[:, layer_idxs[0]:layer_idxs[1]].sum(axis=1) for dataset in datasets]
+            layer_energies[layer] = [dataset[:, layer_idxs[0]:layer_idxs[1]]
+                                     .sum(axis=1) for dataset in datasets]
             
         layer_energies_keys = list(layer_energies.keys())
         
@@ -36,10 +37,13 @@ class ShowerDepthHist(object):
         
         for idx in range(1, len(layer_energies_keys)):
             layer = layer_energies_keys[idx]
-            curr_datasets = [curr_dataset+(layer_dataset*(idx)) for layer_dataset,curr_dataset in zip(layer_energies[layer],curr_datasets)]
+            curr_datasets = [curr_dataset+(layer_dataset*(idx)) for
+                             layer_dataset, curr_dataset in
+                             zip(layer_energies[layer], curr_datasets)]
             
         for dataset in curr_datasets:
             dataset = dataset.reshape(-1)
+            dataset[dataset == 0.] = 1.
             
         datasets = [dataset.sum(axis=1) for dataset in datasets]
         lfracs = [np.divide(curr_dataset, dataset) for curr_dataset, dataset in zip(curr_datasets, datasets)]