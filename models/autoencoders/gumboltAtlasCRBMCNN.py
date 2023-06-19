ffc383c1eab02d54315555c4f50400f30c78eff1
jquetzalcoatl <j.toledo.mx@gmail.com>
2023-06-18 21:27:24 -0700

    new arch

--- models/autoencoders/gumboltAtlasCRBMCNN.py
+++ models/autoencoders/gumboltAtlasCRBMCNN.py
@@ -2,6 +2,8 @@
 GumBolt implementation for Calorimeter data
 CNN - Changed to CNN encoder creation
 """
+# Torch imports
+import torch
 
 # DiVAE.models imports
 from models.autoencoders.gumboltCaloCRBM import GumBoltCaloCRBM
@@ -50,3 +52,59 @@ class GumBoltAtlasCRBMCNN(GumBoltCaloCRBM):
                               activation_fct=self._activation_fct, #<--- try identity
                               num_output_nodes = self._flat_input_size,
                               cfg=self._config)
+    
+    def forward(self, x, is_training):
+        """
+        - Overrides forward in GumBoltCaloV5.py
+        
+        Returns:
+            out: output container 
+        """
+        logger.debug("forward")
+        
+        #see definition for explanation
+        out=self._output_container.clear()
+        
+	    #Step 1: Feed data through encoder
+        in_data = torch.cat([x[0], x[1]], dim=1)
+        out.beta, out.post_logits, out.post_samples = self.encoder(in_data, is_training)
+        post_samples = torch.cat(out.post_samples, 1)
+#         post_samples = torch.cat([post_samples, x[1]], dim=1)
+        
+        output_hits, output_activations = self.decoder(post_samples, x[1])
+        
+        out.output_hits = output_hits
+        beta = torch.tensor(self._config.model.output_smoothing_fct, dtype=torch.float, device=output_hits.device, requires_grad=False)
+        out.output_activations = self._energy_activation_fct(output_activations) * self._hit_smoothing_dist_mod(output_hits, beta, is_training)
+        return out
+    
+    def generate_samples(self, num_samples=64, true_energy=None):
+        """
+        generate_samples()
+        """
+        true_energies = []
+        num_iterations = max(num_samples//self.sampler.get_batch_size(), 1)
+        samples = []
+        for i in range(num_iterations):
+            rbm_visible_samples, rbm_hidden_samples = self.sampler.block_gibbs_sampling()
+            rbm_vis = rbm_visible_samples.detach()
+            rbm_hid = rbm_hidden_samples.detach()
+            
+            if true_energy is None:
+                true_e = torch.rand((rbm_vis.size(0), 1), device=rbm_vis.device).detach() * 100.
+            else:
+                true_e = torch.ones((rbm_vis.size(0), 1), device=rbm_vis.device).detach() * true_energy
+#             prior_samples = torch.cat([rbm_vis, rbm_hid, true_e], dim=1)
+            prior_samples = torch.cat([rbm_vis, rbm_hid], dim=1)
+            
+            output_hits, output_activations = self.decoder(prior_samples, true_e)
+            beta = torch.tensor(self._config.model.beta_smoothing_fct,
+                                dtype=torch.float, device=output_hits.device,
+                                requires_grad=False)
+            sample = self._energy_activation_fct(output_activations) \
+                * self._hit_smoothing_dist_mod(output_hits, beta, False)
+            
+            true_energies.append(true_e) 
+            samples.append(sample)
+            
+        return torch.cat(true_energies, dim=0), torch.cat(samples, dim=0)
