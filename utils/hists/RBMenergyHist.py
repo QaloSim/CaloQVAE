b6180a652a74ebde7ab43b7c80720fc1fdf8262e
jquetzalcoatl <j.toledo.mx@gmail.com>
2024-05-09 15:34:19 -0700

    fixed bin energy wrong call

--- utils/hists/RBMenergyHist.py
+++ utils/hists/RBMenergyHist.py
@@ -48,7 +48,7 @@ def rbm_energy_hist_cond(engine, model_config, val_loader, reducedata=False):
             # beta, post_logits, post_samples = engine.model.encoder(enIn, False)
             beta, post_logits, post_samples = engine.model.encoder(in_data, true_energy, False)
             post_samples = torch.cat(post_samples, 1)
-            u = self.convert_inc_eng_to_binary(true_energy)
+            u = engine.model.convert_inc_eng_to_binary(true_energy)
 
         
             post_samples_energy = engine.model.stater.energy_samples(u, post_samples[:, :partition_size],
